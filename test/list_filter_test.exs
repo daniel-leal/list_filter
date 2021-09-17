0bc3656c4a0a63c8521dcb2e77a14e8b12f74760
Daniel Leal <danielleal94@gmail.com>
2021-09-17 18:12:34 -0300

    Added tests

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -2,7 +2,29 @@ defmodule ListFilterTest do
   use ExUnit.Case
   doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "Returns a new list with the odd numbers" do
+      # Arrange
+      list = ["1", "3", "6", "43", "banana", "6", "abc"]
+      expected_result = [1, 3, 43]
+
+      # Act
+      result = ListFilter.call(list)
+
+      # Assert
+      assert expected_result == result
+    end
+
+    test "Returns an empty list when passed list does not have odd numbers" do
+      # Arrange
+      list = ["6", "banana", "6", "abc"]
+      expected_result = []
+
+      # Act
+      result = ListFilter.call(list)
+
+      # Assert
+      assert expected_result == result
+    end
   end
 end
